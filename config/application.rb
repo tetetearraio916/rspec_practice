340a598f682a17b478e0a5b0ec026c51ff1ad240
tetetearraio916 <notfase1192555@gmail.com>
2021-02-25 15:10:07 +0900

    feat(導入したgemに対して設定を追加):

--- config/application.rb
+++ config/application.rb
@@ -9,18 +9,20 @@ Bundler.require(*Rails.groups)
 module RspecPractice
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 6.1
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       routing_specs: false
+    end
   end
 end
