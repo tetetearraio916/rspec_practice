7923b247e74412db0f159cde4794477eb5ade1bd
tetetearraio916 <notfase1192555@gmail.com>
2021-02-26 07:26:44 +0900

    test(add ユーザー登録のシステムスペックを追加):

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "User", type: :system do
+  describe 'ユーザー登録' do
+    context '入力情報が正しい場合' do
+      it "ユーザー登録ができること" do
+        visit new_user_path
+        fill_in 'ユーザー名', with: 'tetete'
+        fill_in 'メールアドレス', with: 'tetete@example.com'
+        fill_in '年齢', with: 23
+        fill_in 'パスワード', with: 'password'
+        fill_in 'パスワード確認', with: 'password'
+        click_button '登録'
+        expect(current_path).to eq login_path
+        expect(page).to have_content('ユーザーを登録しました')
+      end
+    end
+
+    context '入力情報に誤りがある場合' do
+      it "ユーザー登録ができないこと" do
+        visit new_user_path
+        fill_in 'ユーザー名', with: ''
+        fill_in 'メールアドレス', with: ''
+        fill_in '年齢', with: ''
+        fill_in 'パスワード', with: ''
+        fill_in 'パスワード確認', with: ''
+        click_button '登録'
+        expect(page).to have_content('ユーザー名を入力してください')
+        expect(page).to have_content('メールアドレスを入力してください')
+        expect(page).to have_content('パスワードは3文字以上で入力してください')
+        expect(page).to have_content('パスワード確認を入力してください')
+        expect(page).to have_content('ユーザーの作成に失敗しました')
+      end
+    end
+  end
+end
