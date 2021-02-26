f5f504d9a6c2740f9a9c79d4219b5fef3c71a2ea
tetetearraio916 <notfase1192555@gmail.com>
2021-02-27 04:04:12 +0900

    add blogのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'blogs#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   resources :users, only: %i[new create]
+  resources :blogs
 end
