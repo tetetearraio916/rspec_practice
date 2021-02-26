7fee4732902c6ed12bc40a65da9ee2e47fa5815f
tetetearraio916 <notfase1192555@gmail.com>
2021-02-27 01:18:08 +0900

    add blogsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_061157) do
+ActiveRecord::Schema.define(version: 2021_02_26_155342) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title", limit: 20, null: false
+    t.text "content", limit: 100, null: false
+    t.integer "read", default: 0, null: false
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", limit: 30, null: false
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_02_25_061157) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
