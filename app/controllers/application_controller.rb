7eab8a4902c4692939ce35047cf15016f978a53f
tetetearraio916 <notfase1192555@gmail.com>
2021-02-26 23:12:03 +0900

    feat(add ログインを全ページに要求する機能とログインユーザーを判断するヘルパー実装):

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  before_action :login_required
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+
+  def login_required
+    redirect_to login_path unless current_user
+  end
 end
